bc6a074a0e0c166896cd119017e25b08d9819c18
Aravind662 <122748076+Aravind662@users.noreply.github.com>
2025-07-06 23:53:36 -0500

    minor changes

---
+++ audio.py
@@ -0,0 +1,20 @@
+import whisper
+import os
+import subprocess
+
+def listen():
+    model = whisper.load_model("base")
+    mic_name = "Microphone (Realtek(R) Audio)" 
+
+    print("Listening...")
+    # remove FFmpeg output by redirecting it to null
+    subprocess.run(
+        ['ffmpeg', '-y', '-f', 'dshow', '-i', f'audio={mic_name}', '-t', '5', 'audio.wav'],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+    print("Stopped recording.")
+
+    result = model.transcribe("audio.wav", verbose=False, language="en")
+    print("You said:", result["text"].strip())
+    return result["text"]
